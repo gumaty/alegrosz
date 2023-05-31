78b19e117eaebd418555c4906d468a489a738b1d
Arkadiusz Matlak <piccolo@poczta.fm>
2023-05-31 12:00:18 +0200

    introduce component composition

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,25 @@
+import ProductCard from "./ProductCard.jsx";
+import PropTypes from "prop-types";
+
+function ProductList({ products, search }) {
+    return (
+        <>
+            {products
+                .filter((product) =>
+                    `${product.name} ${product.description}`
+                        .toLowerCase()
+                        .includes(search.toLowerCase())
+                )
+                .map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+        </>
+    );
+}
+
+ProductList.propTypes = {
+    products: PropTypes.array.isRequired,
+    search: PropTypes.string.isRequired
+};
+
+export default ProductList;
